c2d63715ccfa1d692a66b7ae3be71b3ca573d80b
Maina Wycliffe <wmmaina7@gmail.com>
2020-10-28 00:05:44 +0300

    Setup The Basics of a Storage Adapter

---
+++ src/firebase.ts
@@ -0,0 +1,42 @@
+import BaseAdapter, { Image, ReadOptions } from 'ghost-storage-base';
+import { Request, Response, NextFunction } from 'express';
+
+class FirebaseStorageAdapter extends BaseAdapter {
+  constructor() {
+    super();
+  }
+
+  exists(fileName: string, targetDir?: string | undefined): Promise<boolean> {
+    return new Promise((resolve) => {
+      resolve(true);
+    });
+  }
+
+  save(image: Image, targetDir?: string | undefined): Promise<string> {
+    return new Promise((resolve) => {
+      resolve('');
+    });
+  }
+
+  serve() {
+    return function customServe(
+      req: Request,
+      res: Response,
+      next: NextFunction
+    ) {
+      next();
+    };
+  }
+
+  delete(fileName: string, targetDir?: string | undefined): Promise<boolean> {
+    return new Promise((resolve) => {
+      resolve(true);
+    });
+  }
+
+  read(options?: ReadOptions | undefined): Promise<Buffer> {
+    return new Promise((resolve) => {
+      resolve(new Buffer(''));
+    });
+  }
+}
