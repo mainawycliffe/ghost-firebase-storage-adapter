6add07d55fde949d79afc710d1a62ff8964e9f6f
Maina Wycliffe <wmmaina7@gmail.com>
2020-11-01 02:00:45 +0300

    If no-postinstall exist run script

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 const childProcess = require('child_process');
 const fs = require('fs');
 
-if (!fs.existsSync('./no-postinstall')) {
+if (fs.existsSync('./no-postinstall')) {
   childProcess.execSync('node ./lib/postinstall.js');
 }
