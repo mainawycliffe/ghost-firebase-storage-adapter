b9f7c53d8e1ba606e5b0c7688f683fd481de2ed5
Maina Wycliffe <wmmaina7@gmail.com>
2020-10-31 23:56:48 +0300

    Add An Installation Script
    This scripts creates a storage adapter diretory and exports the
    firebase storage adapter

---
+++ src/postinstall.ts
@@ -0,0 +1,22 @@
+import { existsSync, mkdirSync, appendFileSync } from 'fs';
+import { join } from 'path';
+
+const adapterDir = 'content/adapters/storage';
+
+(function setupStorageAdapter(adapterDir: string) {
+  try {
+    // create storage adapter inside the storage adapter directory
+    if (existsSync(join(adapterDir, 'firebase.js'))) {
+      console.warn('A firebase.js exists in the storage adapter directory. Please setup manually!');
+      return;
+    }
+    if (!existsSync(join(__dirname, adapterDir))) {
+      mkdirSync(adapterDir, { recursive: true });
+    }
+    const content = `'use strict'\n\nmodule.exports = require('ghost-firebase-storage-adapter');\n`;
+    appendFileSync(join(adapterDir, 'firebase.js'), content);
+    // add configs to the dev settings
+  } catch (error) {
+    console.error(error);
+  }
+})(adapterDir);
