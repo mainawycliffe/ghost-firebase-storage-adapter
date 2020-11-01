8839a9067efb9dbe9f8f378b86cc130b3b53321a
Maina Wycliffe <wmmaina7@gmail.com>
2020-11-01 18:31:31 +0300

    refactor: convert script into a function

--- script.js
+++ script.js
@@ -1,20 +1,22 @@
 const { exec } = require('child_process');
 const fs = require('fs');
 
-if (fs.existsSync('./.no-postinstall')) {
-  console.log('Post install script was not run');
-  process.exit(0);
-}
-
-console.info('Attempting to configure firebase storage adapter\n\n');
-const postInstall = exec('node ./lib/postinstall.js', function (error, stdout, stderr) {
-  if (error) {
-    console.error(error.stack);
+(function main() {
+  if (fs.existsSync('./.no-postinstall')) {
+    console.log('Post install script was not run');
+    process.exit(0);
   }
-});
 
-postInstall.on('exit', () => {
-  if (postInstall.exitCode === 0) {
-    console.info('You project was configured successfully');
-  }
-});
+  console.info('Attempting to configure firebase storage adapter\n\n');
+  const postInstall = exec('node ./lib/postinstall.js', function (error, stdout, stderr) {
+    if (error) {
+      console.error(error.stack);
+    }
+  });
+
+  postInstall.on('exit', () => {
+    if (postInstall.exitCode === 0) {
+      console.info('You project was configured successfully');
+    }
+  });
+})();
