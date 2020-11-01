b093434943a23e030594ab20badb507a4e6078ed
Maina Wycliffe <wmmaina7@gmail.com>
2020-11-01 15:08:26 +0300

    fix: update Post Install scripts

--- script.js
+++ script.js
@@ -1,6 +1,20 @@
-const childProcess = require('child_process');
+const { exec } = require('child_process');
 const fs = require('fs');
 
-if (fs.existsSync('./no-postinstall')) {
-  childProcess.execSync('node ./lib/postinstall.js');
+if (fs.existsSync('./.no-postinstall')) {
+  console.log('Post install script was not run');
+  process.exit(0);
 }
+
+console.info('Attempting to configure firebase storage adapter\n\n');
+const postInstall = exec('node ./lib/postinstall.js', function (error, stdout, stderr) {
+  if (error) {
+    console.error(error.stack);
+  }
+});
+
+postInstall.on('exit', () => {
+  if (postInstall.exitCode === 0) {
+    console.info('You project was configured successfully');
+  }
+});
